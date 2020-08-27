427e9105866c677262b3b22080388e3a7bbdb84c
Tejas Labde <labdetejas59@gmail.com>
2020-08-27 16:10:36 +0530

    postgres changes 2

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const image=require('./controllers/image');
 const db=knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-triangular-66632',
-      user : 'postgres',
-      password : 'password',
-      database : 'smartbrain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
