712ff7b2994dd6b3a75c421f1293185f9998f875
Tejas Labde <labdetejas59@gmail.com>
2020-08-27 22:56:41 +0530

    image post to put

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req,res)=>{
 app.post('/signin',(req,res) =>{ signin.handleSignin( req , res , db , bcrypt )})
 app.post('/register',(req,res)=>{ register.handleRegister( req , res , db , bcrypt)})
 app.get('/profile/:id', (req,res)=>{ profile.handleProfile( req , res , db )})
-app.post('/image',(req,res)=>{ image.handleImage( req , res, db)  })
+app.put('/image',(req,res)=>{ image.handleImage( req , res, db)  })
 app.post('/imageurl',(req,res)=>{ image.handleApi( req , res)  })
 
 app.listen(process.env.PORT || 3000,()=>{
